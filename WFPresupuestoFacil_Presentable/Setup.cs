60209adb44995cea2deb0c36b4a2b52001a3b352
Samuel Ramirez <samuelramirezsuriel@outlook.com>
2023-10-05 17:33:41 -0400

    fix final

--- WFPresupuestoFacil_Presentable/Setup.cs
+++ WFPresupuestoFacil_Presentable/Setup.cs
@@ -33,7 +33,7 @@ namespace WFPresupuestoFacil_Presentable
             PanelPresupuesto menuPrincipal = new PanelPresupuesto();
             menuPrincipal.lblUser.Text = Global.GlobalVarNombre;
             menuPrincipal.lblPerfil.Text = Global.GlobalVarPerfil;
-            menuPrincipal.ShowDialog();
+            menuPrincipal.Show();
 
 
         }
